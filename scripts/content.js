31dbb1eff6199202a864ffa6b30a060b7ce80d40
Etienne Ott <eti.ott@gmail.com>
2015-10-04 15:56:37 +0200

    Fix issue of hover effect for cards not hovering correctly above the other cards.

--- scripts/content.js
+++ scripts/content.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
     $(window).load(function() {
         // Mark container we're hovering over as active
-        $('.imgContainer, .documentContainer').on('mouseover', function() {
+        $('.imgPlacement, .documentContainer').on('mouseover', function() {
             $(this).addClass('hovering');
         });
 
-        $('.imgContainer, .documentContainer').on('mouseout', function() {
+        $('.imgPlacement, .documentContainer').on('mouseout', function() {
             $(this).removeClass('hovering');
         });
     });
