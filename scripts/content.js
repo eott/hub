eb0452791d13dca09b83dd4ded07daeea0b67a62
Etienne Ott <eti.ott@gmail.com>
2015-10-04 16:28:36 +0200

    Unify card styles and positioning for both images and document cards.

--- scripts/content.js
+++ scripts/content.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
     $(window).load(function() {
         // Mark container we're hovering over as active
-        $('.imgPlacement, .documentContainer').on('mouseover', function() {
+        $('.cardPlacement').on('mouseover', function() {
             $(this).addClass('hovering');
         });
 
-        $('.imgPlacement, .documentContainer').on('mouseout', function() {
+        // Unmark again
+        $('.cardPlacement').on('mouseout', function() {
             $(this).removeClass('hovering');
         });
     });
